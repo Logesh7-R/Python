831f238135d15ec64f18245e03eac5e1993ec579
LOGESH R <159472161+Logesh7-R@users.noreply.github.com>
2025-04-07 08:02:39 +0530

    Add files via upload

---
+++ basics3.py
@@ -0,0 +1,10 @@
+#Escape Sequence
+
+a = 'it\'s sunny'   #single Quotes
+b = "its \"kind of\" sunny"   #Double Quotes
+c = "\t hi !!\nThankYou!!!.."  #Newline and Tab
+d = "Hello \r Hi There!!.."   #Carriage return
+e = "Hello\b madam!!.."  #Backspace
+octl = '\110\123\123\111' #Octal number for character
+hexl = '\x4e\x6f'  #hexagon number for character
+print(a,b,c,d,e,octl,hexl)
