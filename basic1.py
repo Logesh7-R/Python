9bf6c5e56e9a00cecba70251d087a9e959184642
LOGESH R <159472161+Logesh7-R@users.noreply.github.com>
2025-04-08 16:56:57 +0530

    Update basic1.py

--- basic1.py
+++ basic1.py
@@ -5,3 +5,11 @@ stri = ""
 print(bool(age))
 print(bool(mark))
 print(bool(stri))
+
+#Formatted String
+name = "Logesh"
+age = 20
+print(f'hi {name}..your age is {age}')
+print('hi {}..your age is {}'.format(name,age))
+print('hi {1}..your age is {0}'.format(age,name))
+print('hi {name_s}.. your age is {age_s}'.format(age_s=22,name_s="Logi"))
